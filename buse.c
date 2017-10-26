a2ac13ff39b8cae975e34749cf00267e86f83ff4
Jason Mobarak <jason@swiftnav.com>
2017-10-26 10:28:56 -0700

    Don't re-open /dev/nbd0
    If we avoid this code, it doesn't block the open of the server on Ubuntu
    16.04.

--- buse.c
+++ buse.c
@@ -148,11 +148,11 @@ int buse_main(const char* dev_file, const struct buse_operations *aop, void *use
   /* The parent opens the device file at least once, to make sure the
    * partition table is updated. Then it closes it and starts serving up
    * requests. */
-
+#if 0
   tmp_fd = open(dev_file, O_RDONLY);
   assert(tmp_fd != -1);
   close(tmp_fd);
-
+#endif
   close(sp[1]);
   sk = sp[0];
 
